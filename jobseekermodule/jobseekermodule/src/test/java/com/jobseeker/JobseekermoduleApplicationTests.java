7c2c5446165f520c4d9f9fab43c9a64c2d5f88c5
Utkarsh <utkarshbankar07@gmail.com>
2024-08-21 16:59:27 +0530

    First commit

---
+++ jobseekermodule/jobseekermodule/src/test/java/com/jobseeker/JobseekermoduleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jobseeker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobseekermoduleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
